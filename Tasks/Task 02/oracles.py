a8f27fdbe9201cf1e4cf3914be64591c40c1ea22
stchris-ps <75793175+stchris-ps@users.noreply.github.com>
2022-10-29 00:48:02 +0300

    Add_task_2

---
+++ Tasks/Task 02/oracles.py
@@ -0,0 +1,54 @@
+class BaseSmoothOracle:
+    """
+    Базовый класс для реализации оракулов.
+    """
+    def func(self, w):
+        """
+        Вычислить значение функции в точке w.
+        """
+        raise NotImplementedError('Func oracle is not implemented.')
+
+    def grad(self, w):
+        """
+        Вычислить значение градиента функции в точке w.
+        """
+        raise NotImplementedError('Grad oracle is not implemented.')
+
+
+class BinaryLogistic(BaseSmoothOracle):
+    """
+    Оракул для задачи двухклассовой логистической регрессии.
+
+    Оракул должен поддерживать l2 регуляризацию.
+    """
+
+    def __init__(self, l2_coef):
+        """
+        Задание параметров оракула.
+
+        l2_coef - коэффициент l2 регуляризации
+        """
+
+    def func(self, X, y, w):
+        """
+        Вычислить значение функционала в точке w на выборке X с ответами y.
+
+        X - scipy.sparse.csr_matrix или двумерный numpy.array
+
+        y - одномерный numpy array
+
+        w - одномерный numpy array
+        """
+        return super().func(w)
+
+    def grad(self, X, y, w):
+        """
+        Вычислить градиент функционала в точке w на выборке X с ответами y.
+
+        X - scipy.sparse.csr_matrix или двумерный numpy.array
+
+        y - одномерный numpy array
+
+        w - одномерный numpy array
+        """
+        return super().grad(w)
